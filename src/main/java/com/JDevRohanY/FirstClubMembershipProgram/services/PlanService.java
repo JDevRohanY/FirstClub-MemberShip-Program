be10647f174f7f6fbdebe3b493bcae3581667e2a
Rohan Yadav <rohanyadav.career@gmail.com>
2026-05-15 14:43:15 +0530

    Implement getAllPlans endpoint

--- src/main/java/com/JDevRohanY/FirstClubMembershipProgram/services/PlanService.java
+++ src/main/java/com/JDevRohanY/FirstClubMembershipProgram/services/PlanService.java
@@ -1,4 +1,9 @@
 package com.JDevRohanY.FirstClubMembershipProgram.services;
 
+import com.JDevRohanY.FirstClubMembershipProgram.models.MembershipPlan;
+
+import java.util.List;
+
 public interface PlanService {
+    List<MembershipPlan> getAllPlans();
 }
--- src/main/java/com/JDevRohanY/FirstClubMembershipProgram/services/PlanServiceImpl.java
+++ src/main/java/com/JDevRohanY/FirstClubMembershipProgram/services/PlanServiceImpl.java
@@ -1,7 +1,24 @@
 package com.JDevRohanY.FirstClubMembershipProgram.services;
 
+import com.JDevRohanY.FirstClubMembershipProgram.models.MembershipPlan;
+import com.JDevRohanY.FirstClubMembershipProgram.repositories.PlanRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
-public class PlanServiceImpl implements PlanService{
+public class PlanServiceImpl implements PlanService {
+    private final PlanRepository planRepository;
+
+    public PlanServiceImpl(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
+
+    @Override
+    public List<MembershipPlan> getAllPlans() {
+        log.info("Fetching all membership plans");
+        return planRepository.getAllPlans();
+    }
 }
