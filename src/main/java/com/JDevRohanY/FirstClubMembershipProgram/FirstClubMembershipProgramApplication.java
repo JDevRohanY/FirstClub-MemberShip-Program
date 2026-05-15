b8a71259e759aa7a87e6ee45930392fb4f6b9c7a
Rohan Yadav <rohanyadav.career@gmail.com>
2026-05-15 12:54:45 +0530

    Initialized project and setted up mvc structure

---
+++ src/main/java/com/JDevRohanY/FirstClubMembershipProgram/FirstClubMembershipProgramApplication.java
@@ -0,0 +1,13 @@
+package com.JDevRohanY.FirstClubMembershipProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstClubMembershipProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstClubMembershipProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JDevRohanY/FirstClubMembershipProgram/FirstClubMembershipProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JDevRohanY.FirstClubMembershipProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstClubMembershipProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
